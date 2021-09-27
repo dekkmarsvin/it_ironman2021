c935758ce026d78e253321f4cc5c6134373561ad
dekk.Mars <dekkmoor@gmail.com>
2021-09-27 16:05:07 +0800

    test event

--- Server.py
+++ Server.py
@@ -43,6 +43,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    app.logger.debug(f"event:{event}")
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
