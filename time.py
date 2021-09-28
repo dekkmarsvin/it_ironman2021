4a7f710e5404eafd0aa1d7fe15cc1e3ddc1b0707
dekkmarsvin <dekkmoor@gmail.com>
2021-09-28 15:53:19 +0800

    log messaging

--- time.py
+++ time.py
@@ -26,4 +26,4 @@ print("Local time   {}".format(utc_dt.astimezone(get_localzone()).isoformat()))
 print("Local time   {}".format(utc_dt.astimezone(NZST).isoformat()))
 
 ldt = datetime.fromtimestamp(1632791870594 / 1000.0)
-print("TWT time   {}".format(ldt.astimezone(TWT).isoformat()))
+print("TWT time     {}".format(ldt.astimezone(TWT).isoformat(timespec='auto')))
