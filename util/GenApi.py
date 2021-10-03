9ca21c616966a79e6dc4407a388f92bfed09ec77
astlive <irandomsky@gmail.com>
2021-10-03 23:56:03 +0800

    relate

--- util/GenApi.py
+++ util/GenApi.py
@@ -11,8 +11,8 @@ import os
 
 from flask.json import load
 
-from . import APIModel
-from . import APIPm
+from app.util import APIModel
+from app.util import APIPm
 
 cfg = None
 
@@ -163,7 +163,7 @@ if __name__ == '__main__':
                          Api_URL = os.environ['Api_URL'], Nonce_URL = os.environ['Nonce_URL'], BackendURL = os.environ['BackendURL'], \
                         ReturnURL = os.environ['ReturnURL'])
 
-    neworder = APIModel.ReqOrderCreate(ShopNo="NA0249_001", OrderNo="2021100300001", Amount=40400, \
+    neworder = APIModel.ReqOrderCreate(ShopNo="NA0249_001", OrderNo="2021100300002", Amount=40400, \
                 PrdtName="IPhone 13 Pro Max 256g", ReturnURL=cfg.ReturnURL, \
                     BackendURL=cfg.BackendURL, PayType="C", AutoBilling="Y", PayTypeSub="ONE")
     msg, OK = OrderCreate(neworder, cfg)
